281a8b05eb7d69f8fc8fcbd2130c7965989d2a33
shubDhond <shubham.dhond@mail.utoronto.ca>
2016-03-19 21:38:30 -0400

    changing demo1.js

--- js/demo1.js
+++ js/demo1.js
@@ -1,14 +1,25 @@
 (function() {
-	var triggerBttn = document.getElementById( 'trigger-overlay' ),
-		overlaymatcostpre = document.querySelector( 'div.overlay-matcostpre' ),
+	var overlaymatcostpre = document.querySelector( 'div.overlay-matcostpre' ),
 		overlaywarehousingpre = document.querySelector( 'div.overlay-warehousingpre'),
 		overlaytransportationcu = document.querySelector( 'div.overlay-transportationcu'),
 		overlaylabourcu = document.querySelector( 'div.overlay-labourcu'),
 		overlaytimecu = document.querySelector( 'div.overlay-timecu'),
 		overlaytransportationpost = document.querySelector( 'div.overlay-transportationpost'),
 		overlaydisposalpost = document.querySelector( 'div.overlay-disposalpost'),
-
-		closeBttn = overlay.querySelector( 'button.overlay-close' );
+		triggermatcostpre = document.querySelector( 'matcostpre'),
+		triggerwarehousingpre = document.querySelector( 'warehousingpre'),
+		triggertransportationcu = document.querySelector( 'transportationcu'),
+		triggerlabourcu = document.querySelector( 'labourcu'),
+		triggertimecu = document.querySelector( 'timecu'),
+		triggertransportationpost = document.querySelector( 'transportationpost'),
+		triggerdisposalpost = document.querySelector( 'disposalpost'),
+		closematcostpre = document.querySelector( 'overlay-close-matcostpre'),
+		closewarehousingpre = document.querySelector( 'overlay-close-warehousingpre'),
+		closetransportationcu = document.querySelector( 'overlay-close-transportationcu'),
+		closelabourcu = document.querySelector( 'overlay-close-labourcu'),
+		closetimecu = document.querySelector( 'overlay-close-timecu'),
+		closetransportationpost = document.querySelector( 'overlay-close-transportationpost'),
+		closedisposalpost = document.querySelector( 'overlay-close-disposalpost');
 		transEndEventNames = {
 			'WebkitTransition': 'webkitTransitionEnd',
 			'MozTransition': 'transitionend',
@@ -42,6 +53,18 @@
 		}
 	}
 
-	triggerBttn.addEventListener( 'click', toggleOverlay );
-	closeBttn.addEventListener( 'click', toggleOverlay );
+	triggermatcostpre.addEventListener('click', toggleOverlay(overlaymatcostpre));
+	closematcostpre.addEventListener('click', toggleOverlay(overlaymatcostpre));
+	triggerwarehousingpre.addEventListener('click', toggleOverlay(overlaywarehousingpre));
+	closewarehousingpre.addEventListener('click', toggleOverlay(overlaywarehousingpre));
+	triggerlabourcu.addEventListener('click', toggleOverlay(overlaylabourcu));
+	closelabourcu.addEventListener('click', toggleOverlay(overlaylabourcu));
+	triggertransportationcu.addEventListener('click', toggleOverlay(overlaytransportationcu));
+	closetransportationcu.addEventListener('click', toggleOverlay(overlaytransportationcu));
+	triggertimecu.addEventListener('click', toggleOverlay(overlaytimecu));
+	closetimecu.addEventListener('click', toggleOverlay(overlaytimecu));
+	triggertransportationpost.addEventListener('click', toggleOverlay(overlaytransportationpost));
+	closetransportationpost.addEventListener('click', toggleOverlay(overlaytransportationpost));
+	triggerdisposalpost.addEventListener('click', toggleOverlay(overlaydisposalpost));
+	closedisposalpost.addEventListener('click', toggleOverlay(overlaydisposalpost));
 })();
